690014ba6ddec33f70764edc80d798ed26463314
wrdowney <willd20@vt.edu>
2023-08-02 22:20:40 -0400

    style adjustments

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,8 +4,8 @@ import "monkey/token"
 
 type Lexer struct {
     input        string
-    position     int // current position in input (points to current char)
-    readPosition int // current reading position in input (after current char)
+    position     int  // current position in input (points to current char)
+    readPosition int  // current reading position in input (after current char)
     ch           byte // current char under examination
 }
 
